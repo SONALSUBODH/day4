7747868f753c84f9bb661bcd7be58e6655a9018b
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-20 22:40:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -60,10 +60,28 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "web" {
-  ami             = var.ami_id
+  ami             = data.aws_ami.ubuntu.id
   instance_type   = var.instance_type[0]
   security_groups = [aws_security_group.tf_sg.name]
   tags            = var.instance_tags
   
 
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+output "instance_ip_addr" {
+  value = aws_instance.demo.public_ip
 }
