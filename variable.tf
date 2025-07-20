ee442aa6c50e7e614977938ebf3d41631353cbbe
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-20 18:45:39 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -14,3 +14,25 @@ variable "instance_tags" {
     Team  = "DevOps"
   }
 }
+variable "sg_name" {
+type     = string
+default  = "tf_sg1"
+}
+variable "sg_description" {
+type     = string
+default  = "Allow TLS inbound traffic"
+}
+variable "vpc_id" {
+type     = string
+default  = "vpc-05bdcc8880aab85ab"
+
+}
+
+variable "sg_tags" {
+type     = map
+default = {
+    Name  = "Allow Tls"
+    Ports = "WEBSERVER PORT"
+  }
+
+
